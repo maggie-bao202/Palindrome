2bc7517421bcbfc4d4807566517751a28b1be750
maggie-bao202 <maggie.bao202@gmail.com>
2019-09-17 14:30:25 -0700

    Add files via upload

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,26 +1,52 @@
 #include <iostream>
 #include <cstring>
+
 using namespace std;
 int main()
 {
   char str[80];
   char str2[80];
+  char str3[80];
   cin.get(str, 80);
   cin.get();
-
-  int count = 0;
+  bool go = true;
   
   for (int i = 0; i < strlen(str); i++){
-    if (
+    if( str[i] =='\0'){
+      str2[i] = '\0';
+      break;
+    }
     if (isalnum(toupper(str[i])) == true){
-      str[i] == str2[i];
+      str2[i] = str[i];
       
-    }  
+    }
   }
   cout << str2 << endl;
-  
-  for (int i = strlen(str) - 1; i >= 0; i--){
-    cout << str2[i];
+
+  int j = 0;
+  for (int i = strlen(str2) - 1; i >= 0; i--){
+    str3[j] = str2[i];
+    j++;
   }
+
+  cout << str3 << endl;
+
+  for (int i = 0; i < strlen(str2); i++){
+    for (int j = 0; j < strlen(str3); j++){
+      if (str3[j] == str2[i]){
+	go = true;
+      }
+      else {
+	go = false;
+	break;
+      }
+    }
+  }
+  if (go = true){
+    cout << "This is a Palindrome!" << endl;
+  }
+  else {
+    cout << "Not a Palindrome." << endl;
+  }  
   return 0;
 }

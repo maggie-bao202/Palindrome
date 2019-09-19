98015da0d74afc0e62d8e1473d4ea888ecf3c012
U-BSD\463293 <463293@SHS-10DEC7FA8.beaverton.k12.or.us>
2019-09-19 09:13:26 -0700

    final

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -2,6 +2,8 @@
 #include <cstring>
 
 using namespace std;
+/*9/19/19 Author: Maggie Bao Description: The user is prompted to input a sentence. The computer will then tell the user whether or not they entered a palindrome-- a sentence or word that is  the same forwards as it is backwards.*/
+
 int main()
 {
   char str[80];
@@ -9,44 +11,44 @@ int main()
   char str3[80];
   cin.get(str, 80);
   cin.get();
-  bool go = true;
   
+  int count = 0; //keep count of index after removing spaces
+  for (int i = 0; str[i]; i++){ //Removes spaces in input
+    if( str[i] != ' '){
+      str[count++] = str[i];
+    }
+  } 
+  str[count] = '\0'; //nulls last character
+  
+  //cout <<str << endl;
+
+  count = 0; //reset count to zero to keep track of new array str2 index
   for (int i = 0; i < strlen(str); i++){
-    if( str[i] =='\0'){
-      str2[i] = '\0';
-      break;
-    }
-    if (isalnum(toupper(str[i])) == true){
-      str2[i] = str[i];
-      
+    if (isalnum(toupper(str[i])) == true){//create array str2 that only includes numbers/letters
+      str[i] = tolower(str[i]);//convert all letters to lowercase
+      str2[count++] = str[i]; //put into str2 array
+
     }
   }
-  cout << str2 << endl;
+ 
+  str2[count] = '\0'; //null last char
+  
+  //cout << str2 << endl;
 
-  int j = 0;
-  for (int i = strlen(str2) - 1; i >= 0; i--){
-    str3[j] = str2[i];
-    j++;
+  count = 0;
+  for (int i = strlen(str2) - 1; i >= 0; i--){//read last character in array, move forward
+    str3[count++] = str2[i];//put in new array str3
   }
+  str3[count] = '\0';
 
-  cout << str3 << endl;
+  //cout << str3 << endl;
 
-  for (int i = 0; i < strlen(str2); i++){
-    for (int j = 0; j < strlen(str3); j++){
-      if (str3[j] == str2[i]){
-	go = true;
-      }
-      else {
-	go = false;
-	break;
-      }
-    }
-  }
-  if (go = true){
+  if (strcmp (str2, str3) == 0){//if matches
     cout << "This is a Palindrome!" << endl;
-  }
-  else {
-    cout << "Not a Palindrome." << endl;
   }  
+  else {
+    cout << "This is not a Palindrome." << endl;
+  }
+  
   return 0;
 }
